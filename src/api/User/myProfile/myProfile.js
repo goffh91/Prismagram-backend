a72794ff4624a1eab5f8daa6a67bb38bf0ee5320
KyunWuPark <goffh91@naver.com>
2019-09-20 20:52:53 +0900

    .

--- src/api/User/myProfile/myProfile.js
+++ src/api/User/myProfile/myProfile.js
@@ -1,17 +1,11 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { USER_FRAGMENT } from "../../../fragments";
 
 export default {
     Query: {
         myProfile: async (_, args, { request, isAuthenticated }) => {
             isAuthenticated(request);
             const { user } = request;
-            const myProfile = await prisma.user({ id: user.id });
-            const myPosts = await prisma.user({ id: user.id }).posts();
-            return {
-                user: myProfile, 
-                posts: myPosts
-            };
+            return await prisma.user({ id: user.id });
         }
     }
 }
